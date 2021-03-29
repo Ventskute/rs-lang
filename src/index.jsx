afdb2f29a9825c8143d3fe1ee14d02346932512d
Tatsiana Slapik <slapik.tatiana@gmail.com>
2021-03-30 00:20:56 +0300

    feat: added backend call, parameters matching, diffc level switcher

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { Provider, useDispatch, useSelector } from "react-redux";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
@@ -14,35 +14,24 @@ export default function App() {
   const store = createStore(rootReducer, composeWithDevTools());
 
   return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route path="/savanna" component={Savanna}>
-            <Savanna
-              words={[
-                { word: "baker's", translation: "булочная" },
-                { word: "bookshop", translation: "книжный магазин" },
-                { word: "butcher's", translation: "мясной магазин" },
-                { word: "clothes shop", translation: "магазин одежды" },
-                { word: "dry-cleaner's", translation: "химчистка" },
-
-                { word: "electronics shop", translation: "магазин элетроники" },
-
-                { word: "greengrocer's", translation: "овощной магазин" },
-                { word: "hairdresser's", translation: "парикмахерская" },
-                { word: "internet cafe", translation: "интернет кафе" },
-                { word: "pharmacy", translation: "аптека" },
-
-                { word: "newsagent's", translation: "газетный киоск" },
-                { word: "shoe shop", translation: "обувной магазин" },
-                { word: "sports shop", translation: "спортивный магазин" },
-                { word: "supermarket", translation: "супермаркет" },
-              ]}
-            />
-          </Route>
-        </Switch>
-      </Router>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route path={"/savanna"} exact component={Savanna} />
+            <Route path={"/savanna/:group/:page"} exact component={Savanna} />
+          </Switch>
+          <ul>
+            <li>
+              <Link to="/savanna">Component</Link>
+            </li>
+            <li>
+              <Link to="/savanna/1/2">From page</Link>
+            </li>
+          </ul>
+        </Router>
+      </Provider>
+    </>
   );
 }
 
--- src/сomponents/Savanna/Savanna.jsx
+++ src/сomponents/Savanna/Savanna.jsx
@@ -1,12 +1,105 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 import "./Savanna.scss";
-let previousIndexes = [];
 
-function RandomIndex(seed) {
+export default function Savanna() {
+  const [words, setWords] = useState([]);
+  const [randomWords, setRandomWords] = useState([]);
+  const [word, setWord] = useState();
+  const [livesCount, setLivesCount] = useState(5);
+  const [difficLevel, setDifficLevel] = useState(1);
+  let { group, page } = useParams();
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:3000/words?group=${group ? group : difficLevel}&page=${
+        page ? page : 1
+      }`
+    )
+      .then((res) => res.json())
+      .then((words) => {
+        setWords(words);
+        let randWords = GetRandomWords(words);
+        setWord(randWords.pop());
+        setRandomWords(randWords);
+      });
+  }, []);
+
+  return (
+    <div className="savanna">
+      {group && page ? (
+        ""
+      ) : (
+        <div>
+          <select
+            onChange={(e) => {
+              setDifficLevel(e.target.value);
+            }}
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+          </select>
+        </div>
+      )}
+      <div className="lives-counter">{livesCount}</div>
+      {livesCount == 0 ? (
+        <div> YOU LOST!</div>
+      ) : (
+        <>
+          {word ? (
+            <>
+              <div className="word">
+                <p>{word.word}</p>
+              </div>
+              <ul className="words-list">
+                {GetRandomAnswers(word.wordTranslate, words).map((elem, i) => {
+                  return (
+                    <li
+                      onClick={() => {
+                        if (elem === word.wordTranslate) {
+                          setWord(randomWords.pop());
+                          setRandomWords(randomWords);
+                        } else {
+                          setWord(randomWords.pop());
+                          setRandomWords(randomWords);
+                          setLivesCount(livesCount - 1);
+                        }
+                      }}
+                      key={i}
+                    >
+                      {elem}
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          ) : (
+            ""
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+function GetRandomWords(words) {
+  let previousIndexes = [];
+  let randomWords = [];
+  for (let i = 0; i < words.length; i++) {
+    randomWords.push(words[RandomIndex(previousIndexes, words.length)]);
+  }
+  return randomWords;
+}
+
+function RandomIndex(previousIndexes, seed) {
   let index = Math.floor(Math.random() * seed);
   if (previousIndexes.includes(index)) {
-    return RandomIndex(seed);
+    return RandomIndex(previousIndexes, seed);
   }
   previousIndexes.push(index);
   return index;
@@ -17,60 +110,14 @@ function GetRandomAnswers(answer, words) {
   do {
     const rand = Math.floor(Math.random() * words.length);
     let t = answers.filter((v) => {
-      return v === words[rand].translation;
+      return v === words[rand].wordTranslate;
     });
     if (t.length == 0) {
-      answers.push(words[rand].translation);
+      answers.push(words[rand].wordTranslate);
     }
   } while (answers.length < 4);
 
   return shuffle(answers);
-}
-
-let livesCount = 5;
-
-export default function Savanna(props) {
-  let index;
-  if (previousIndexes.length === 0) {
-    index = RandomIndex(props.words.length);
-  }
-  const [word, setWord] = useState(props.words[index]);
-
-  let answers = GetRandomAnswers(word.translation, props.words);
-
-  return (
-    <div className="savanna">
-      <div className="lives-counter">{livesCount}</div>
-      {livesCount == 0 ? (
-        <div> YOU LOST!</div>
-      ) : (
-        <>
-          <div className="word">
-            <p>{word.word}</p>
-          </div>
-          <ul className="words-list">
-            {answers.map((elem, i) => {
-              return (
-                <li
-                  onClick={() => {
-                    if (elem === word.translation) {
-                      setWord(props.words[RandomIndex(props.words.length)]);
-                    } else {
-                      setWord(props.words[RandomIndex(props.words.length)]);
-                      livesCount--;
-                    }
-                  }}
-                  key={i}
-                >
-                  {elem}
-                </li>
-              );
-            })}
-          </ul>
-        </>
-      )}
-    </div>
-  );
 }
 
 function shuffle(array) {
