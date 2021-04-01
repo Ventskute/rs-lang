66bd7abc6d91d71736a88d59ea0a854b06484209
AlekseiBY <aleksei.kupchinskii@gmail.com>
2021-04-01 21:43:38 +0300

    feat: add game to index jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,13 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import SprintGameMenu from './components/SprintGameMenu/SprintGameMenu'
-// import AudioChallenge from "./components/AudioChallenge/AudioChallenge";
+import SprintGameMenu from "./components/SprintGameMenu/SprintGameMenu";
+import AudioChallengeContainer from "./components/AudioChallenge/AudioChallengeContainer";
 import { UserUpdater } from "./components/UserUpdater/UserUpdater";
 import Main from "./views/Main/Main";
 
 import rootReducer from "./utils/rootReducer";
-import sprintReducer from './utils/reducerSprint'
-import './index.scss'
+import "./index.scss";
 import Fillwords from "./views/Fillwords/Fillwords";
 
 import "./index.scss";
@@ -26,8 +25,8 @@ export default function App() {
           <Switch>
             <Route path="/sprint" component={SprintGameMenu} />
             <Route path="/" exact component={Main} />
-            {/* <Route path="/audioChallenge" component={AudioChallenge} /> */}
             <Route path="/fillwords" component={Fillwords} />
+            <Route path="/audioChallenge" component={AudioChallengeContainer} />
           </Switch>
         </Router>
       </UserUpdater>
