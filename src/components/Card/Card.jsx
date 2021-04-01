e95667f7886e62253e9b1c0b395f946fa6daaddc
Viktoryia Ventskute <ventskute.vika@gmail.com>
2021-04-01 16:23:33 +0300

    feat: add cards content

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Badge, Button, Card } from 'react-bootstrap';
 import { getStaticURL } from '../../utils/api';
 
 import './Card.scss';
 
-export default function Cards({word, image, textExample}) {
+const playAudio = (audio) => {
+  new Audio(getStaticURL(audio)).play();
+}
+
+export default function Cards({word, image, textExample, transcription, wordTranslate, textMeaning, textMeaningTranslate, textExampleTranslate, audio, audioExample, audioMeaning}) {
   return (
     <Card>
       <Card.Img variant="top" src={getStaticURL(image)} />
       <Card.Body>
         <Card.Title>{word}</Card.Title>
-        <Card.Text dangerouslySetInnerHTML={{__html: textExample}}></Card.Text>
-        <Button variant="primary">There will be an audio</Button>
+        <div className='text-button'>
+          <Card.Text>{transcription}</Card.Text>
+          <Button variant="primary" className='play' onClick={() => playAudio(audio)}>▶</Button>
+        </div>
+        <Card.Text>{wordTranslate}</Card.Text>
+        <div className='text-button'>
+          <Card.Text dangerouslySetInnerHTML={{__html: textMeaning}}></Card.Text>
+          <Button variant="primary" className='play' onClick={() => playAudio(audioMeaning)}>▶</Button>
+        </div>
+        <Card.Text>{textMeaningTranslate}</Card.Text>
+        <div className='text-button'>
+          <Card.Text dangerouslySetInnerHTML={{__html: textExample}}></Card.Text>
+          <Button variant="primary" className='play' onClick={() => playAudio(audioExample)}>▶</Button>
+        </div>
+        <Card.Text>{textExampleTranslate}</Card.Text>
      </Card.Body>
     </Card>
   )
