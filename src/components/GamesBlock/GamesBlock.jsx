8b268433c032d04449f7d2cfdc0e0ca24af657e9
Your Name <Anamay94.am@gmail.com>
2021-04-02 16:24:32 +0300

    feat: new cards for gamesBlock

--- src/components/GamesBlock/GamesBlock.jsx
+++ src/components/GamesBlock/GamesBlock.jsx
@@ -2,16 +2,24 @@ import './GamesBlock.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { gamesArr } from '../../utils/games/blockArrays';
+import { Button, Card } from 'react-bootstrap';
+
+import cardImage from '../../assets/images/background.jpg';
 
 function GamesBlock(onClick) {
   return (
     <div className="games">
-      {gamesArr.map(({ name, className, link }, i) => (
-        <Link to={`${link}`} key={i}>
-          <div key={i} className={`game-item games__${className}`} onClick={(e) => console.log(e)}>
-            {name}
-          </div>
-        </Link>
+      {gamesArr.map(({ name, className, link, description }, i) => (
+        <Card className={`game-item games__${className}`} style={{ width: '18rem' }} key={i}>
+          <Card.Img variant="top" src={cardImage} />
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>{description}</Card.Text>
+            <Link to={`${link}`} key={i}>
+              <Button variant="primary">Играть</Button>
+            </Link>
+          </Card.Body>
+        </Card>
       ))}
     </div>
   );
--- src/utils/games/blockArrays.js
+++ src/utils/games/blockArrays.js
@@ -2,21 +2,25 @@ export const gamesArr = [
     {
       name: 'Саванна',
       className: '__savanna',
-      link: './savanna'
+      link: './savanna',
+      description: 'Здесь будет описание геймплея игры.'
     },
     {
       name: 'Спринт',
       className: '__sprint',
-      link: './sprint'
+      link: './sprint',
+      description: 'Игра, в которой необходимо указать, верен ли перевод слов за отведенное время.'
     },
     {
       name: 'Филворды',
       className: '__fillwords',
-      link: './fillwords'
+      link: './fillwords',
+      description: 'Здесь будет описание геймплея игры.'
     },
     {
       name: 'Аудиовызов',
       className: '__audio-call',
-      link: './audioChallenge'
+      link: './audioChallenge',
+      description: 'Здесь будет описание геймплея игры.'
     },
   ];
