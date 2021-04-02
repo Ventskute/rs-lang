52f317b5dd9d6e264f7116c58be51e74c302d54a
TatsianaSlapik <70775751+TatsianaSlapik@users.noreply.github.com>
2021-04-02 17:51:05 +0300

    Savanna (#4)
    * feat: Added styles to savanna component
    
    * feat: added backend call, parameters matching, diffc level switcher
    
    * Merge remote-tracking branch 'origin/dev' into savanna
    
    * feat: added some styles
    
    * feat: small updates
    
    * feat: added statistics game
    
    * feat: added game components
    
    * feat: fixed bug
    
    * feat: fixed problem
    
    Co-authored-by: Mikita Liaukovich <Mikita_Liaukovich@epam.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import SprintGameMenu from './components/SprintGameMenu/SprintGameMenu'
+import SprintGameMenu from "./components/SprintGameMenu/SprintGameMenu";
 // import AudioChallenge from "./components/AudioChallenge/AudioChallenge";
 import { UserUpdater } from "./components/UserUpdater/UserUpdater";
 import Main from "./views/Main/Main";
-
-import rootReducer from "./utils/rootReducer";
-import sprintReducer from './utils/reducerSprint'
-import './index.scss'
 import Fillwords from "./views/Fillwords/Fillwords";
 
+import rootReducer from "./utils/rootReducer";
+import sprintReducer from "./utils/reducerSprint";
+
 import "./index.scss";
+import SavannaMenu from "./components/Savanna/SavannaMenu/SavannaMenu";
+import Savanna from "./components/Savanna/Savanna";
 
 export default function App() {
   const store = createStore(rootReducer, composeWithDevTools());
@@ -24,6 +25,9 @@ export default function App() {
       <UserUpdater>
         <Router>
           <Switch>
+            <Route path={"/savannaMenu"} exact component={SavannaMenu} />
+            <Route path={"/savanna"} exact component={Savanna} />
+            <Route path={"/savanna/:group/:page"} exact component={Savanna} />
             <Route path="/sprint" component={SprintGameMenu} />
             <Route path="/" exact component={Main} />
             {/* <Route path="/audioChallenge" component={AudioChallenge} /> */}
