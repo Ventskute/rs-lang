09d2d31a881176767dc2ab4c449415ed0221a294
Viktoryia Ventskute <ventskute.vika@gmail.com>
2021-04-03 20:31:33 +0300

    feat: add modal

--- src/components/Textbook/SettingsBlock/SettingsBlock.jsx
+++ src/components/Textbook/SettingsBlock/SettingsBlock.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import actions from '../../../utils/actions';
 import './SettingsBlock.scss';
 import settingsIcon from '../../../assets/icons/settings.png';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 
-function SettingsBlock() {
+const SettingsModal = (props) => {
   const { settings } = useSelector(state => state);
   const dispatch = useDispatch();
 
-  const [showModal, setShowModal] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setShowAnim(false), 2000)
+  }, [])
 
   const onCheckedInput = (e) => {
     dispatch({
@@ -20,55 +22,51 @@ function SettingsBlock() {
     })
   };
 
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Настройки
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="textbook__settings">
+          <Form>
+            <Form.Check
+              type='checkbox'
+              name="translations"
+              checked={settings.translations}
+              onChange={onCheckedInput}
+              label={`Отображать перевод слов/предложений`}
+              />
+            <Form.Check
+              type='checkbox'
+              name="buttons"
+              checked={settings.buttons}
+              onChange={onCheckedInput}
+              label={`Отображать кнопки-опции слов`}
+            />
+          </Form>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Закрыть</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
-  const SettingsModal = (props) => {
-    return (
-      <Modal
-        {...props}
-        show={showModal}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Настройки
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="textbook__settings">
-            <div className="settings translation-settings">
-              <input
-                className="settings__checkbox"
-                name="translations"
-                type="checkbox"
-                onChange={(e) => onCheckedInput(e)}
-                checked={settings.translations}
-              />
-              <span className="settings__checkbox_description">Отображать перевод слов/предложений</span>
-            </div>
-            <div className="settings button-settings">
-              <input
-                className="settings__checkbox"
-                name="buttons"
-                type="checkbox"
-                onChange={(e) => onCheckedInput(e)}
-                checked={settings.buttons}
-              />
-              <span className="settings__checkbox_description">Отображать кнопки-опции слов</span>
-            </div>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Закрыть</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+function SettingsBlock() {
+  const [showModal, setShowModal] = useState(false);
 
   return (<>
     <img src={settingsIcon} alt="settings" className='settings--img' onClick={() => setShowModal(true)}/>
-    <SettingsModal onHide={() => setShowModal(false)}/>
+    <SettingsModal show={showModal} onHide={() => setShowModal(false)}/>
   </>);
 }
 
