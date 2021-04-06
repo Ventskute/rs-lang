23fa001202872bd6502a3118e66664286bd414ac
Your Name <Anamay94.am@gmail.com>
2021-04-06 17:01:36 +0300

    feat: LS

--- src/components/Textbook/Textbook.jsx
+++ src/components/Textbook/Textbook.jsx
@@ -11,12 +11,20 @@ import GamesBlock from '../GamesBlock/GamesBlock';
 import WordsList from '../WordsList/WordsList';
 import { Container } from 'react-bootstrap';
 import Footer from '../Footer/Footer';
+import { textBookLS } from '../../utils/localStore';
 
 function Textbook() {
   const textBookSections = Array(6).fill(null);
-  const [difficulty, setDifficulty] = React.useState('0');
-  const [page, setPage] = React.useState({ currentPage: 1 });
+  const [difficulty, setDifficulty] = React.useState(
+    textBookLS.getTextBookLS() ? textBookLS.getTextBookLS()[0] : 0,
+  );
+  const [page, setPage] = React.useState(
+    textBookLS.getTextBookLS() ? textBookLS.getTextBookLS()[1] : { currentPage: 1 },
+  );
 
+  React.useEffect(() => {
+    textBookLS.setTextBook(page, difficulty);
+  }, [page, difficulty]);
   const onClickCategory = (e) => {
     setDifficulty(e);
     console.log(e);
@@ -33,7 +41,10 @@ function Textbook() {
         <div className="sections textbook__sections">
           <h2 className="sections__title">Секции сложности</h2>
           <div className="sections__content">
-            <Tabs id="controlled-tab-example" activeKey={difficulty} onSelect={(e) => onClickCategory(e)}>
+            <Tabs
+              id="controlled-tab-example"
+              activeKey={difficulty}
+              onSelect={(e) => onClickCategory(e)}>
               {textBookSections.map((el, i) => (
                 <Tab key={i} eventKey={i} title={i + 1}>
                   <Sonnet />
@@ -42,7 +53,7 @@ function Textbook() {
             </Tabs>
           </div>
         </div>
-        <WordsList page={page.currentPage} difficulty={difficulty} />
+        <WordsList page={page.currentPage - 1} difficulty={difficulty} />
         <PaginationWordList state={page} setState={setPage} />
         <GamesBlock />
       </Container>
