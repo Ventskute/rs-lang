64de330793e1c1b87dd2e44fbd8beb6cbb7fecf1
AlekseiBY <aleksei.kupchinskii@gmail.com>
2021-04-14 12:44:48 +0300

    feat: add dictionary

---
+++ src/components/Dictionary/Dictionary.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { Tabs, Tab } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { getDeletedWords, getNormalWords, getHardWords } from "../../utils/api/api";
+import TabPages from "./TabPages/TabPages";
+import LearningWords from "./LearningWords/LearningWords";
+
+import "./Dictionary.scss";
+
+const Dictionary = () => {
+  const { user } = useSelector((state) => state);
+  const initialWords = {
+    learning: [],
+    hard: [],
+    deleted: [],
+  };
+  const [words, setWords] = useState(initialWords);
+  useEffect(async () => {
+    const learning = await getNormalWords(user.userId);
+    const hard = await getHardWords(user.userId);
+    const deleted = await getDeletedWords(user.userId);
+
+    setWords({ deleted, hard, learning });
+  }, []);
+  return (
+    <div className="dictionary">
+      <Tabs className="dictionary-tabs">
+        <Tab eventKey="learning" title="learning">
+          {words.learning[0] && (
+            <TabPages words={words.learning} WordsListImplementation={LearningWords} />
+          )}
+        </Tab>
+        <Tab eventKey="hard" title="hard">
+          {words.hard[0] && <TabPages incomingWords={words.hard} />}
+        </Tab>
+        <Tab eventKey="deleted" title="deleted">
+          {words.deleted[0] && <TabPages incomingWords={words.deleted} />}
+        </Tab>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Dictionary;
---
+++ src/components/Dictionary/LearningWords/LearningWords.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Accordion, Button, Card, Container } from "react-bootstrap";
+import Cards from "../../Card/Card";
+
+const LearningWords = ({ incomingWords }) => {
+  const { translations, buttons } = useSelector((state) => state.settings);
+  const [words, setWords] = useState(incomingWords);
+  useEffect(() => {
+    if (incomingWords) {
+      setWords(incomingWords);
+    }
+  }, [incomingWords]);
+
+  return (
+    <Container>
+      <Accordion>
+        {words &&
+          words.map((el, i) => {
+            return (
+              <Card key={i} className="card-collapsed" bg={false ? "danger" : "light"}>
+                <Accordion.Toggle as={Card.Header} eventKey={i + 1} className="wordlist-item">
+                  <p>{el.word}</p>
+                  <p>{el.transcription}</p>
+                  {translations && <p>{el.wordTranslate}</p>}
+                  <p>{el.userWord.optional.rightAnswersCount}</p>
+                  <p>{el.userWord.optional.wrongAnswersCount}</p>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={i + 1}>
+                  <div className="content">
+                    <Card.Body>{Cards(el)}</Card.Body>
+                    {buttons && (
+                      <div className="buttons-wrapper">
+                        <Button className="button-action">Добавить в раздел "Сложные слова"</Button>
+                        <Button className="button-action">Удалить</Button>
+                      </div>
+                    )}
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+            );
+          })}
+      </Accordion>
+    </Container>
+  );
+};
+
+export default LearningWords;
