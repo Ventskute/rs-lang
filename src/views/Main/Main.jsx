f68258b557564894a84457bb4c001adfffb46b89
Viktoryia Ventskute <45541398+Ventskute@users.noreply.github.com>
2021-04-17 20:41:23 +0300

    feat: add video

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -61,8 +61,8 @@ export default function Main() {
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/t8k71QcArnk"
-            title="YouTube video player"
+            src="https://www.youtube.com/embed/PizLiqTrbck"
+            title="RS Lang demo"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen></iframe>
