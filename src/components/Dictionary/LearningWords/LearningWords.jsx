14e0f7c2c65226ef438da72e48b648e164a82ec6
AlekseiBY <aleksei.kupchinskii@gmail.com>
2021-04-18 00:01:47 +0300

    fix: revmove settings from dictionary

--- src/components/Dictionary/LearningWords/LearningWords.jsx
+++ src/components/Dictionary/LearningWords/LearningWords.jsx
@@ -4,7 +4,6 @@ import { Accordion, Button, Card, Container } from "react-bootstrap";
 import Cards from "../../Card/Card";
 
 export const DictionaryWords = ({ incomingWords, restoreHandler, deleteHandler, wordType }) => {
-  const { translations, buttons } = useSelector((state) => state.settings);
   const [words, setWords] = useState(incomingWords);
   const { user } = useSelector((state) => state);
   useEffect(() => {
@@ -21,33 +20,37 @@ export const DictionaryWords = ({ incomingWords, restoreHandler, deleteHandler, 
             const isHard = el.userWord && el.userWord.difficulty !== "normal";
             const isDeleted = el.userWord && el.userWord.optional.wordStatus === "deleted";
             return (
-              <Card key={i} className="card-collapsed" bg={isHard ? "danger" : "light"} text={isHard ? "light" : "dark"}>
+              <Card
+                key={i}
+                className="card-collapsed"
+                bg={isHard ? "danger" : "light"}
+                text={isHard ? "light" : "dark"}
+              >
                 <Accordion.Toggle as={Card.Header} eventKey={i + 1} className="wordlist-item">
                   <p>{el.word}</p>
                   <p>{el.transcription}</p>
-                  {translations && <p>{el.wordTranslate}</p>}
+                  <p>{el.wordTranslate}</p>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey={i + 1}>
                   <div className="content">
                     <Card.Body>{Cards(el)}</Card.Body>
-                    {buttons && (
-                      <div className="buttons-wrapper">
+
+                    <div className="buttons-wrapper">
+                      <Button
+                        onClick={() => restoreHandler(user.userId, el._id, wordType)}
+                        className="button-action"
+                      >
+                        Восстановить
+                      </Button>
+                      {!isDeleted && (
                         <Button
-                          onClick={() => restoreHandler(user.userId, el._id, wordType)}
+                          onClick={() => deleteHandler(user.userId, el._id, wordType)}
                           className="button-action"
                         >
-                          Восстановить
+                          Удалить
                         </Button>
-                        {!isDeleted && (
-                          <Button
-                            onClick={() => deleteHandler(user.userId, el._id, wordType)}
-                            className="button-action"
-                          >
-                            Удалить
-                          </Button>
-                        )}
-                      </div>
-                    )}
+                      )}
+                    </div>
                   </div>
                 </Accordion.Collapse>
               </Card>
@@ -81,41 +84,44 @@ const LearningWords = ({
           words.map((el, i) => {
             const isHard = el.userWord && el.userWord.difficulty !== "normal";
             return (
-              <Card key={i} className="card-collapsed" bg={isHard ? "danger" : "light"} text={isHard ? "light" : "dark"}>
+              <Card
+                key={i}
+                className="card-collapsed"
+                bg={isHard ? "danger" : "light"}
+                text={isHard ? "light" : "dark"}
+              >
                 <Accordion.Toggle as={Card.Header} eventKey={i + 1} className="wordlist-item">
                   <p>{el.word}</p>
                   <p>{el.transcription}</p>
-                  {translations && <p>{el.wordTranslate}</p>}
+                  <p>{el.wordTranslate}</p>
                   <p className="correct-answer">{el.userWord.optional.rightAnswersCount}</p>
                   <p className="wrong-answer">{el.userWord.optional.wrongAnswersCount}</p>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey={i + 1}>
                   <div className="content">
                     <Card.Body>{Cards(el)}</Card.Body>
-                    {buttons && (
-                      <div className="buttons-wrapper">
+                    <div className="buttons-wrapper">
+                      <Button
+                        onClick={() => restoreHandler(user.userId, el._id, wordType)}
+                        className="button-action"
+                      >
+                        Восстановить
+                      </Button>
+                      {!isHard && (
                         <Button
-                          onClick={() => restoreHandler(user.userId, el._id, wordType)}
+                          onClick={() => addToHardHandler(user.userId, el._id, wordType)}
                           className="button-action"
                         >
-                          Восстановить
+                          Добавить в раздел "Сложные слова"
                         </Button>
-                        {!isHard && (
-                          <Button
-                            onClick={() => addToHardHandler(user.userId, el._id, wordType)}
-                            className="button-action"
-                          >
-                            Добавить в раздел "Сложные слова"
-                          </Button>
-                        )}
-                        <Button
-                          onClick={() => deleteHandler(user.userId, el._id, wordType)}
-                          className="button-action"
-                        >
-                          Удалить
-                        </Button>
-                      </div>
-                    )}
+                      )}
+                      <Button
+                        onClick={() => deleteHandler(user.userId, el._id, wordType)}
+                        className="button-action"
+                      >
+                        Удалить
+                      </Button>
+                    </div>
                   </div>
                 </Accordion.Collapse>
               </Card>
