340c005d1a06d2f3676713963174c9234f7499fd
AlekseiBY <aleksei.kupchinskii@gmail.com>
2021-04-17 08:43:45 +0300

    fix: fix merge conflicts

--- src/components/Dictionary/LearningWords/LearningWords.jsx
+++ src/components/Dictionary/LearningWords/LearningWords.jsx
@@ -3,7 +3,69 @@ import { useSelector } from "react-redux";
 import { Accordion, Button, Card, Container } from "react-bootstrap";
 import Cards from "../../Card/Card";
 
-const LearningWords = ({ incomingWords }) => {
+export const DictionaryWords = ({ incomingWords, restoreHandler, deleteHandler, wordType }) => {
+  const { translations, buttons } = useSelector((state) => state.settings);
+  const [words, setWords] = useState(incomingWords);
+  const { user } = useSelector((state) => state);
+  useEffect(() => {
+    if (incomingWords) {
+      setWords(incomingWords);
+    }
+  }, [incomingWords]);
+
+  return (
+    <Container>
+      <Accordion>
+        {words &&
+          words.map((el, i) => {
+            const isHard = el.userWord && el.userWord.difficulty !== "normal";
+            const isDeleted = el.userWord && el.userWord.optional.wordStatus === "deleted";
+            return (
+              <Card key={i} className="card-collapsed" bg={isHard ? "danger" : "light"}>
+                <Accordion.Toggle as={Card.Header} eventKey={i + 1} className="wordlist-item">
+                  <p>{el.word}</p>
+                  <p>{el.transcription}</p>
+                  {translations && <p>{el.wordTranslate}</p>}
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={i + 1}>
+                  <div className="content">
+                    <Card.Body>{Cards(el)}</Card.Body>
+                    {buttons && (
+                      <div className="buttons-wrapper">
+                        <Button
+                          onClick={() => restoreHandler(user.userId, el._id, wordType)}
+                          className="button-action"
+                        >
+                          Восстановить
+                        </Button>
+                        {!isDeleted && (
+                          <Button
+                            onClick={() => deleteHandler(user.userId, el._id, wordType)}
+                            className="button-action"
+                          >
+                            Удалить
+                          </Button>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </Accordion.Collapse>
+              </Card>
+            );
+          })}
+      </Accordion>
+    </Container>
+  );
+};
+
+const LearningWords = ({
+  incomingWords,
+  restoreHandler,
+  deleteHandler,
+  addToHardHandler,
+  wordType,
+}) => {
+  const { user } = useSelector((state) => state);
   const { translations, buttons } = useSelector((state) => state.settings);
   const [words, setWords] = useState(incomingWords);
   useEffect(() => {
@@ -24,21 +86,34 @@ const LearningWords = ({ incomingWords }) => {
                   <p>{el.word}</p>
                   <p>{el.transcription}</p>
                   {translations && <p>{el.wordTranslate}</p>}
-                  <p>{el.userWord.optional.rightAnswersCount}</p>
-                  <p>{el.userWord.optional.wrongAnswersCount}</p>
+                  <p className="correct-answer">{el.userWord.optional.rightAnswersCount}</p>
+                  <p className="wrong-answer">{el.userWord.optional.wrongAnswersCount}</p>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey={i + 1}>
                   <div className="content">
                     <Card.Body>{Cards(el)}</Card.Body>
                     {buttons && (
                       <div className="buttons-wrapper">
-                        {isHard && <Button className="button-action">Восстановить</Button>}
+                        <Button
+                          onClick={() => restoreHandler(user.userId, el._id, wordType)}
+                          className="button-action"
+                        >
+                          Восстановить
+                        </Button>
                         {!isHard && (
-                          <Button className="button-action">
+                          <Button
+                            onClick={() => addToHardHandler(user.userId, el._id, wordType)}
+                            className="button-action"
+                          >
                             Добавить в раздел "Сложные слова"
                           </Button>
                         )}
-                        <Button className="button-action">Удалить</Button>
+                        <Button
+                          onClick={() => deleteHandler(user.userId, el._id, wordType)}
+                          className="button-action"
+                        >
+                          Удалить
+                        </Button>
                       </div>
                     )}
                   </div>
